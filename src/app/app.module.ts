06dd9e9c9de999c9aee6b3eb9eec1ff418097ed4
Rajvel CG <cgrvel@gmail.com>
2019-07-20 21:01:47 +0530

    completed till 12
    This reverts commit ea228b0dedc8ad663395c801a69e79758f804298.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
+} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +31,8 @@ import { HeaderComponent } from './header/header.component';
     MatCardModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector : 'app-post-list',
-  templateUrl : './post-list.component.html'
+  templateUrl : './post-list.component.html',
+  styleUrls : ['./post-list.component.css']
 })
 
-export class PostCreateComponent {
+export class PostListComponent {
 
+  // posts = [
+  //   {title : 'First Post', content : 'Harry potter'},
+  //   {title : 'Second Post', content : 'Voila'},
+  //   {title : 'Third Post', content : 'Dota'},
+  // ];
+  @Input() posts = [];
 }
