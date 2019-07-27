a87ceb93e394d7d8e9282a11165b56ba3602c569
Rajvel CG <cgrvel@gmail.com>
2019-07-28 01:15:06 +0530

    07 Fetching intial posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require ('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-  console.log('first');
-  next();
-});
-
-app.use((req, res, next)=>{
-  res.send('Hellow from express!');
+app.use('/api/posts',(req, res, next)=>{
+  const posts =[
+    {
+      id:"1234",
+      title: "Harry potter",
+      content: "Part 1"
+    },
+    {
+      id:"5678",
+      title: "Harry potter",
+      content: "Part 2"
+    }
+  ]
+  res.status(200).json({
+    message: 'Post sent Successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
