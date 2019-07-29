b03bf2258ccdd7d66f2486da7a85541070f9c7f7
Rajvel CG <cgrvel@gmail.com>
2019-07-29 23:28:50 +0530

    07 Made changes till editing the post

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -32,6 +32,10 @@ export class PostService {
     return this.postUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return {...this.posts.find(p => p.id === id)};
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
@@ -49,6 +53,14 @@ export class PostService {
       const updatedPost = this.posts.filter(post => post.id !== postId);
       this.posts = updatedPost;
       this.postUpdated.next([...this.posts]);
+    });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {id: id, title: title, content: content};
+    this.http.put('http://localhost:3000/api/posts/' + id, post)
+    .subscribe(response => {
+      console.log(response);
     });
   }
 }
