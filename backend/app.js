73f4ad3c355331454675d2d8f60deb0de02f8959
Rajvel CG <cgrvel@gmail.com>
2019-08-02 00:24:53 +0530

    09 made server side good

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const app = express();
-const Post = require ('./models/post');
+const postsRoutes = require ('./routes/posts');
 const mongoose = require ('mongoose');
 
 mongoose.connect('mongodb://localhost/node-angular', { useNewUrlParser: true })
@@ -12,6 +12,8 @@ mongoose.connect('mongodb://localhost/node-angular', { useNewUrlParser: true })
 .catch((err) => {
   console.log('Connection failed',err);
 });
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -30,67 +32,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts',(req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post Added Successfully',
-      postId: createdPost._id
-    });
-  });
-
-});
-
-app.get('/api/posts',(req, res, next)=>{
-  Post.find()
-  .then(documents => {
-    console.log('get: ',documents);
-    res.status(200).json({
-      message: 'Post sent Successfully',
-      posts: documents
-    });
-  });
-});
-
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if(post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({
-        message: 'Post not found!',
-      });
-    }
-  });
-});
-
-app.delete('/api/posts/:id',(req, res, next) => {
-  Post.deleteOne({_id: req.params.id})
-  .then(result => {
-    console.log('delete: ',result);
-    res.status(200).json({
-      message: 'Post Deleted'
-    });
-  });
-});
-
-app.put('/api/posts/:id',(req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id }, post)
-  .then(result => {
-    console.log('put: ',result);
-    res.status(200).json({
-      message: 'Post Updated'
-    });
-  });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
