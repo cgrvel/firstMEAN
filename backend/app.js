9990c79cc41623633ddb4dcb56b32693f78d9830
Rajvel CG <cgrvel@gmail.com>
2019-09-12 14:34:00 +0530

    Cource Completed

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,8 @@ const userRoutes = require ('./routes/user');
 const mongoose = require ('mongoose');
 const path = require ('path');
 
-mongoose.connect('mongodb://localhost/node-angular', { useNewUrlParser: true })
-//mongoose.connect('mongodb+srv://game:+ process.env.MONGO_ATLAS_PWD +@cluster0-v9f6w.mongodb.net/node-angular?retryWrites=true&w=majority', { useNewUrlParser: true })
+//mongoose.connect('mongodb://localhost/node-angular', { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://game:" + process.env.MONGO_ATLAS_PWD + "@cluster0-v9f6w.mongodb.net/node-angular?retryWrites=true&w=majority", { useNewUrlParser: true })
 .then(() => {
   console.log('Connected to database');
 })
@@ -19,7 +19,8 @@ mongoose.connect('mongodb://localhost/node-angular', { useNewUrlParser: true })
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin','*');
@@ -37,5 +38,7 @@ app.use((req, res, next) => {
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
-
+app.use((req, res, next) => {
+  res.sendfile(path.join(__dirname, "angular", "index.html"));
+});
 module.exports = app;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: "http://nodeangular-env-2.kvmfk7e2um.us-east-2.elasticbeanstalk.com/api"
 };
